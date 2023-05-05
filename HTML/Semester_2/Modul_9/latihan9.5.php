d5744f28c38a15b0dde22d8737c11d883587e64b
Naia Shaffa <naiashaffa.2022@student.uny.ac.id>
2023-05-05 15:15:28 +0700

    modul 9 5/5

---
+++ HTML/Semester_2/Modul_9/latihan9.5.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Radio Button</title>
+</head>
+<body>
+    
+<form action="<?php $_SERVER['PHP_SELF'];?>" method="post" >
+    Jenis Kelamin
+    <br />
+    <input type="radio" name="input_gender" value="Pria"/> Pria
+    <input type="radio" name="input_gender" value="Wanita"/> Wanita
+    <br />
+
+    <input type="submit" value="OK"/>
+</form>
+
+<?php 
+echo '<br />';
+if (isset($_POST['input_gender'])) {
+    echo $_POST['input_gender'];
+}
+?>
+</body>
+</html>
