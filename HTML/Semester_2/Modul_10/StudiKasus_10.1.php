1228b0227a0f358a76b5c11ac31599a8eead39a6
Naia Shaffa <naiashaffa.2022@student.uny.ac.id>
2023-05-15 21:02:11 +0700

    new project studikasus 15/5

---
+++ HTML/Semester_2/Modul_10/StudiKasus_10.1.php
@@ -0,0 +1,77 @@
+<?php 
+    session_start();
+    if (isset($_POST['login'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        //periksa login
+        if ($username == "admin" && $password == "12345") {
+            // menciptakan session
+            $_SESSION['login'] = $username;
+            //menuju ke halaman pemeriksaan session
+            echo "<p align=center>Anda berhasil Login!</p>";
+        }
+    } else {
+        ?>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+            <!-- Bootstrap 5 CSS -->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+                integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" 
+                integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+            </script>
+            
+            <title>Log In Page</title>
+
+            <!-- JavaScript-->
+            <script>
+                // Fungsi untuk validasi form
+                function validateForm() {
+                    var username = document.getElementById("input_username").value;
+                    var password = document.getElementById("input_password").value;
+                    if (username == "" || password == "") { 
+                        alert("Username dan Password harus diisi."); 
+                        document.getElementById("input_username").focus(); 
+                        document.getElementById("input_username").select(); 
+                        return false; 
+                    }
+                    if (!/^[a-zA-Z]+$/.test(username) || !/^[a-zA-Z]+$/.test(password)) { 
+                        document.getElementById("input_username").focus(); 
+                        document.getElementById("input_username").select(); 
+                        return false; 
+                    }
+                    return true; 
+                }
+            </script>
+        </head>
+
+        <body>
+            <!-- Form HTML -->
+            <div class="container-fluid text-center col-13 col-md-8 col-lg-4">
+                <div class="card" style="margin-top: 80px; margin-bottom: 60px;">
+                    <h2 style="margin-top: 20px; margin-bottom: 20px;">LOG IN</h2>
+
+                    <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post" onsubmit="return validateForm()">
+                        <div class="container text-center">
+                            <div>
+                                <input class="form-control" type="text" name="username" id="Username" placeholder="Username" style="margin-bottom: 12px;">
+                            </div>
+                            <div>
+                                <input class="form-control" type="password" name="password" id="Password" placeholder="Password">
+                            </div>
+                        </div>
+                        <input type="submit" value="Log In" style="margin-top: 25px; margin-bottom: 25px;">
+                    </form>
+                </div>
+            </div>
+            
+        </body>
+        </html>
+    <?php
+    }
+?>
